7e30e90dcf1150a9d88ba56b3f0e6a2668ff7a12
argaen <manu.mirandad@gmail.com>
2026-08-31 20:46:54 +0800

    Improve gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import type { StaticImageData } from 'next/image';
 
 import {
+  blurPlaceholderProps,
   IMAGE_QUALITY_SECTION,
   IMAGE_SIZES_GALLERY,
 } from '@/lib/next-image';
@@ -15,7 +16,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from '@/components/ui/carousel';
 
 export type GalleryProps = {
   arrowClassName: string;
@@ -25,29 +26,31 @@ export type GalleryProps = {
     alt: string;
     caption: string;
   }[];
-}
+};
 
 export default function Gallery({
   images,
   arrowClassName,
 }: GalleryProps) {
-
   return (
     <div className="flex flex-col items-center justify-center text-white-water">
-       <Carousel className="w-8/12 md:w-3/5">
-        <CarouselContent>
+      <Carousel className="w-8/12 md:w-3/5">
+        <CarouselContent className="ml-0">
           {images.map((img, i) => (
-            <CarouselItem key={i} className="rounded-md">
-              <Image
-                src={img.src}
-                alt={img.alt}
-                width={1200}
-                height={800}
-                quality={IMAGE_QUALITY_SECTION}
-                sizes={IMAGE_SIZES_GALLERY}
-                loading="lazy"
-                className="block w-full h-auto rounded-md object-cover"
-              />
+            <CarouselItem key={`${img.src}-${i}`} className="basis-full pl-0">
+              <div className="relative aspect-[3/2] w-full overflow-hidden rounded-md bg-black-sand/40">
+                <Image
+                  src={img.src}
+                  alt={img.alt}
+                  fill
+                  quality={IMAGE_QUALITY_SECTION}
+                  sizes={IMAGE_SIZES_GALLERY}
+                  priority={i === 0}
+                  loading="eager"
+                  className="object-cover"
+                  {...blurPlaceholderProps(img.src)}
+                />
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
